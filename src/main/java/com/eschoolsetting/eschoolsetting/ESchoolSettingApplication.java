5cd5730c8c15cf12aa7c0c9158a24984aa181f5c
MandeepSharm <mansharma90@gmail.com>
2023-08-17 21:15:45 +0530

    add new setting module

---
+++ src/main/java/com/eschoolsetting/eschoolsetting/ESchoolSettingApplication.java
@@ -0,0 +1,13 @@
+package com.eschoolsetting.eschoolsetting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ESchoolSettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ESchoolSettingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eschoolsetting/eschoolsetting/ESchoolSettingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eschoolsetting.eschoolsetting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ESchoolSettingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
